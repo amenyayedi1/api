8d38c9f61180f1172a4f2470ece668ce4143cb9b
amenyayedi <amenyayedi@amenyayedis-MacBook-Air.local>
2024-01-27 12:27:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function app(){
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getUsers = ()=> {
+       fetch('https://jsonplaceholder.typicode.com/users')
+       .then((res) => res.json())
+       .then((data)=> setUsers(data))
+       .catch((err) => console.log(err));
+    };
+    getUsers();
+    setLoading(false);
+    console.log(users);
+  },[])
+  return(
+  <div className="App">
+    {loading ? <h1>loading...</h1> : 
+    users.map((el) => 
+    <div style={{border:'1px solid black'}}> 
+      <h3> {el.email} </h3>
+    <p> {el.name} </p>
+    <p> {el.adress.street} </p>
     </div>
+   )
+    }
+    
+  </div>
   );
 }
 
-export default App;
+export default app;
